54fb2f46b110ef77f72348ba4978f08bc51c3bfb
Nikolas Santana de Arruda <nsarruda@mtrix.com.br>
2025-02-05 16:49:38 -0300

    feat: change paste estruture

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  test: {
+    globals: true,
+  }
+})
